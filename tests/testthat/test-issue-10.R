ae3f0624572d82ec4e08d9c8bf6a66e352c349d6
Moritz Beller <moritzbeller@gmx.de>
2017-02-08 14:34:31 +0100

    Make test fail

--- tests/testthat/test-issue-10.R
+++ tests/testthat/test-issue-10.R
@@ -5,7 +5,7 @@ test_that("Bug #10 is fixed", {
 
   dates <- c("", "2014-10-29", "2012-12-05")
   pd <- parse_date(dates)
-  pd2 <- c(parse_date(""),
+  pd2 <- c(parse_date("2012"),
            parse_date("2014-10-29"),
            parse_date("2012-12-05")
            )
